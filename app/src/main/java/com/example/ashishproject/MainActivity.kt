b41019f3fd61f841ec94ad6b0c21ae93855ee253
ashis <ashishratnaraj@gmail.com>
2023-05-15 11:51:05 +0530

    Added view binding commit and push

--- app/src/main/java/com/example/ashishproject/MainActivity.kt
+++ app/src/main/java/com/example/ashishproject/MainActivity.kt
@@ -4,29 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Message
 import android.util.Log
+import com.example.ashishproject.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        fun getCircleArea(side1:Int,side2:Int) {
-            val Area = side1*side2
-            Log.i("DEMO", Area.toString())
 
-        val names = listOf("ashish","Abhinav","Martin",)
-        val cities= listOf("Dehradun","Delhi","Chennai")
-        val movies = listOf("Superman","Spiderman","Batman")
+    }
+}
 
-        val area = getCircleArea(1.0)
-        Log.i("DEMO","The area of the circle \nwith a 1.0 radius is $area")
-        val area1 = getCircleArea(3.6)
-        Log.i("DEMO","The area of the circle \nwith a 3.6 radius is $area")
-        val area2 = getCircleArea(10)
-        Log.i("DEMO","The area of the circle \nwith a 10 radius is $area")
-
-        printList(names)
-        printList(cities)
-        printList(movies)
 
 
 
